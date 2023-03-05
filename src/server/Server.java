5d16db94bfc6a19f932464303f6cc6c7645348df
Vladlen <vladlen@vozhzhaev.ru>
2023-03-05 11:33:35 +0300

    mutlithread

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,16 +11,30 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(9178);
             System.out.println("Сервер запущен");
-            // Ждём клиента и сохраняем его в socket
-            Socket socket = serverSocket.accept();
-            System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             while (true){
-                String clientMessage = in.readUTF();
-                System.out.println(clientMessage);
-                out.writeUTF(clientMessage.toUpperCase()); // Сервер отправляет сообщение
+                // Ждём клиента и сохраняем его в socket
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключился");
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DataInputStream in = new DataInputStream(socket.getInputStream());
+                            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                            while (true){
+                                String clientMessage = in.readUTF();
+                                System.out.println(clientMessage);
+                                out.writeUTF(clientMessage.toUpperCase()); // Сервер отправляет сообщение
+                            }
+                        }catch (IOException e){
+                            System.out.println("Потеряно соединение с клиентом");
+                        }
+
+                    }
+                });
+                thread.start();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
